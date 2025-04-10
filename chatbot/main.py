9d6f038d7e5653d1f82c5ee9a6f8e84548fe5858
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2025-04-10 11:24:50 -0300

    Chatbot with language set

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,7 +1,5 @@
 from src.chatbot import invoke
 
 if __name__ == "__main__":
-  thread_id = "chatbot_test_1"
-
-  invoke("Hi! I'm Gabriel.", thread_id)
-  invoke("What is my name?", thread_id)
+  invoke("Hi! I'm Gabriel.", "Italian")
+  invoke("What is my name?")
--- chatbot/src/chatbot.py
+++ chatbot/src/chatbot.py
@@ -1,27 +1,64 @@
+from typing import Sequence
+from typing_extensions import Annotated, TypedDict
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
 from langchain.chat_models import init_chat_model
 from langgraph.checkpoint.memory import MemorySaver
-from langgraph.graph import START, MessagesState, StateGraph
-from langchain_core.messages import HumanMessage
+from langgraph.graph import START, StateGraph
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 model = init_chat_model("gpt-4o-mini", model_provider="openai")
 
+thread_id = "chatbot_test_1"
+
+
+class State(TypedDict):
+  messages: Annotated[Sequence[BaseMessage], add_messages]
+  language: str
+
+
 # Define a new graph
-workflow = StateGraph(state_schema=MessagesState)
+workflow = StateGraph(state_schema=State)
+
+prompt_template = ChatPromptTemplate.from_messages(
+  [
+    (
+      "system",
+      "You are a helpful assistant. Answer all questions to the best of your ability in {language}."
+      # "You talk like a pirate. Answer all questions to the best of your ability.",
+    ),
+    MessagesPlaceholder(variable_name="messages"),
+  ]
+)
 
 
 # Define the function that calls the model
-def call_model(state: MessagesState):
-  response = model.invoke(state["messages"])
+def call_model(state: State):
+  prompt = prompt_template.invoke(state)
+  response = model.invoke(prompt)
+
   return {"messages": response}
 
 
-def invoke(query: str, thread_id: str):
+def invoke(query: str, language: str = ""):
   if query and thread_id:
     config = {"configurable": {"thread_id": thread_id}}
 
     input_messages = [HumanMessage(query)]
 
-    output = app.invoke({"messages": input_messages}, config)
+    for chunk, metadata in app.stream(
+      {"messages": input_messages, "language": language},
+      config,
+      stream_mode="messages",
+    ):
+      if isinstance(chunk, AIMessage):  # Filter to just model responses
+        print(chunk.content, end="|")
+
+    if language:
+      output = app.invoke({"messages": input_messages, "language": language}, config)
+    else:
+      output = app.invoke({"messages": input_messages}, config)
 
     output["messages"][-1].pretty_print()  # output contains all messages in state
 
