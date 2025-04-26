34bf4af7ce031a4509d70d3e9fc2a62f61b663c0
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2025-04-26 16:35:36 -0300

    Endpoint address for chatting

--- rag-front/app/page.tsx
+++ rag-front/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/ask", {
+      const res = await fetch("http://localhost:5000/api/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question }),
